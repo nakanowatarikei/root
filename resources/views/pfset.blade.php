f84cc25a814e6e7e22c0e2840d8662b7b3c27ac8
Kei Nakanowatari <nakanowatari.key@gmail.com>
2019-07-03 15:22:33 +0900

    ロート定義とhtmlの修正

--- resources/views/pfset.blade.php
+++ resources/views/pfset.blade.php
@@ -1,111 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
-</head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+<!-- resources/views/books.blade.php -->
+@extends('layouts.app')
+@section('content')
 
     <!-- @yield('content') -->
 <div class="container">
-    <form>
+<form action="{{ url('pfsets') }}" method="POST" class="form-horizontal" enctype="multipart/form-data">
+            {{ csrf_field() }}
         <div class="form-group">
-            <label for="exampleInputEmail1">name</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="name">
+            <label for="exampleInputEmail1">名前</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="名前" name="name">
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" name="email">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="Password">
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">Youtube</label>
-            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="Youtube">
+            <label for="exampleInputEmail1">facebookのURL</label>
+            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="facebook" name="facebook">
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">Twitter</label>
-            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="Twitter">
+            <label for="exampleInputEmail1">TwitterのURL</label>
+            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="Twitter" name="twtter">
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">instagram</label>
-            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="instagram">
+            <label for="exampleInputEmail1">instagramのURL</label>
+            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="instagram" name="Password">
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">Qiita</label>
-            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="Qiita">
+            <label for="exampleInputEmail1">QiitaのURL</label>
+            <input type="url" class="form-control" id="exampleInputEmail1" placeholder="Qiita" name="Password">
         </div>
         <div class="form-group">
-            <label for="exampleInputFile">File input</label>
-            <input type="file" id="exampleInputFile">
+            <label for="exampleInputEmail1">プロフィール説明文</label>
+            <textarea class="form-control" rows="3" placeholder="プロフィール説明文" name="Password"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputFile">プロフィール画像</label>
+            <input type="file" id="exampleInputFile" name="image" capture="camera">
             <p class="help-block">Example block-level help text here.</p>
         </div>
         <div class="checkbox">
@@ -115,6 +50,7 @@
         </div>
         <button type="submit" class="btn btn-primary btn-lg">Submit</button>
     </form>
+    @endsection
 </div>
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// プロフィール登録のポストルート定義
+Route::post('/pfsets', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:64',
+        'email' => 'required|max:256',
+        'Password' => 'required|max:64',
+        'image' => 'required',
+        'twtter' => 'required',
+        'facebook' => 'required',
+        'instagram' => 'instagram',
+        'image' => 'required',
+        'qiita' => 'required',
+        'profile' => 'required',
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    //以下に登録処理を記述（Eloquentモデル）
+});
