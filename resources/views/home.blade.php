c9e5eca3a339345915e552c56d2d56191a54b544
ar22ok <ari.k.1222@gmail.com>
2019-07-05 17:51:14 +0900

    プロフィール上部再送

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
-@section('')
-
-@endsection
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,5 @@
         </main>
         
     </div>
-    <!-- @yield('home.js') -->
 </body>
 </html>
